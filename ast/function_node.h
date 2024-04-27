1d4730d9f2ee296c3fb7f1d049bf66ce093b172e
Gonçalo Azevedo <goncalo.r.azevedo@tecnico.ulisboa.pt>
2024-04-27 11:43:59 +0100

    fix(ast): fix `function_node` and `declaration_node`

--- ast/function_node.h
+++ ast/function_node.h
@@ -4,23 +4,43 @@
 #include "block_node.h"
 #include <cdk/ast/expression_node.h>
 #include <cdk/ast/sequence_node.h>
+#include <cdk/types/basic_type.h>
+#include <cdk/types/functional_type.h>
+#include <cdk/types/primitive_type.h>
+#include <vector>
 
 namespace til {
 
 /**
  * Class for describing function definition nodes.
  */
-class function_node : public cdk::typed_node {
+class function_node : public cdk::expression_node {
     cdk::sequence_node *_arguments;
     til::block_node *_block;
+    bool _main = false;
 
   public:
-    function_node(int lineno,
-                             std::shared_ptr<cdk::basic_type> func_type,
-                             cdk::sequence_node *arguments,
-                             til::block_node *block)
-        : cdk::typed_node(lineno), _arguments(arguments), _block(block) {
-        type(func_type);
+    function_node(int lineno, std::shared_ptr<cdk::basic_type> func_type,
+                  cdk::sequence_node *arguments, til::block_node *block)
+        : cdk::expression_node(lineno), _arguments(arguments), _block(block) {
+        std::vector<std::shared_ptr<cdk::basic_type>> arg_types;
+        for (size_t i; i < arguments->size(); i++) {
+            arg_types.push_back(
+                dynamic_cast<cdk::typed_node *>(arguments->node(i))->type());
+        }
+
+        type(cdk::functional_type::create(arg_types, func_type));
+    }
+
+    /**
+     * Main function node constructor
+     */
+    function_node(int lineno, til::block_node *block)
+        : cdk::expression_node(lineno),
+          _arguments(new cdk::sequence_node(lineno)), _block(block),
+          _main(true) {
+        type(cdk::functional_type::create(
+            cdk::primitive_type::create(4, cdk::TYPE_INT)));
     }
 
   public:
